2afb0dfd6cc07a00efca2102599517f0bf3c183a
Ayat Tulloh Rahulloh Khomeini <5026211156@mhs.its.ac.id>
2024-05-06 17:34:12 +0700

    chore: Update face cascade classifier file path and download if missing

--- computer_Visualization/face_recognition_simulation/face_dataset.py
+++ computer_Visualization/face_recognition_simulation/face_dataset.py
@@ -1,19 +1,26 @@
 import cv2
 import os
+import urllib.request
 
-face_cascade = cv2.CascadeClassifier('../haarcascade_frontalface_default.xml')
+cascade_file = 'haarcascade_frontalface_default.xml'
+if not os.path.exists(cascade_file):
+    url = 'https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml'
+    urllib.request.urlretrieve(url, cascade_file)
+
+face_cascade = cv2.CascadeClassifier(cascade_file)
 cap = cv2.VideoCapture(0)
 dataset_path='dataset/'
 
 if not os.path.exists(dataset_path):
-    os.makedirs(dataset_path)
+    os.mkdir(dataset_path)
 
 person_id = 1
 count = 0
 while True:
     _, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(frame, 1.3, 5, minSize=(30, 30))
+  
+    faces = face_cascade.detectMultiScale(frame, 1.1, 5, minSize=(30, 30))
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
